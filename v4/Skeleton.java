aaebb60c911a046f29e28584dfa273c016e16250
JaylenZeng <jzeng30@stuy.edu>
2021-11-23 22:38:43 -0500

    Update Skeleton.java

--- v4/Skeleton.java
+++ v4/Skeleton.java
@@ -1,6 +1,10 @@
 public class Skeleton extends Monster {
     public Skeleton() {
-        _attack = .4;
+        super();
+        _hitPts = 100;
+        _strength = (int)(Math.random() * 60) + 30;
+        _defense = 30;
+        _attack = .1;
         _name = "Skeleton";
     }
 
@@ -12,5 +16,5 @@ public class Skeleton extends Monster {
     public static String about() {
         return "Skeletons art tough and sloweth, but those gents art eke v'ry weak";
     }
-    
+
 }
